56e7a243cc62d5a859fab2f197ff1df3fb86e565
dos <noblesse721@gmail.com>
2018-07-23 11:42:51 +0900

    공통 DML 작업 모듈 추가
    반복적인 DML 함수 코딩작업을 줄이기 위해 제작

---
+++ src/main/java/egovframework/survey/comn/util/ISurveyCommonDAO.java
@@ -0,0 +1,10 @@
+package egovframework.survey.comn.util;
+
+import java.util.Map;
+
+public interface ISurveyCommonDAO {
+	public void insert (Map<String,Object> map) throws Exception;
+	public void update (Map<String,Object> map) throws Exception;
+	public void delete (Map<String,Object> map) throws Exception;
+	public Map<String,Object> select (Map<String,Object> map) throws Exception;
+}
