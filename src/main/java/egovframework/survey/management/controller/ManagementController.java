8b754fa67721ad47af5571ab420ae65a32cead1b
dos <noblesse721@gmail.com>
2018-07-23 11:55:48 +0900

    망했다..

--- src/main/java/egovframework/survey/management/controller/ManagementController.java
+++ src/main/java/egovframework/survey/management/controller/ManagementController.java
@@ -1,13 +1,29 @@
 package egovframework.survey.management.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import egovframework.survey.management.service.ManagementService;
 
 @Controller
 public class ManagementController {
 	
+	@Autowired
+	public ManagementService service;
+	
 	@RequestMapping(value="/survey/management.do")
-	public String index(){
+	public String index() throws Exception{
+		return "/management/index";
+	}
+	
+	@RequestMapping(value="/survey/regForm.do")
+	public String regForm() throws Exception{
+		return "/management/regForm";
+	}
+	
+	@RequestMapping(value="/survey/reg.do")
+	public String regSruvey() throws Exception{
 		return "/management/index";
 	}
 	
