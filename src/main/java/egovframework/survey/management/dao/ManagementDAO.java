9f49316c86e7a94df9dd26aac152a29dbaf53cfd
dos <noblesse721@gmail.com>
2018-07-25 17:41:19 +0900

    파일명 변경으로 삭제 커밋

--- src/main/java/egovframework/survey/management/dao/ManagementDAO.java
+++
@@ -1,9 +0,0 @@
-package egovframework.survey.management.dao;
-
-import egovframework.rte.psl.dataaccess.mapper.Mapper;
-import egovframework.survey.comn.util.ISurveyCommonDAO;
-
-@Mapper(value= "egovframework.survey.management.dao.ManagementDAO")
-public interface ManagementDAO extends ISurveyCommonDAO{
-	
-}
